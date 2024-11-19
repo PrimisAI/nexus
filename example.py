54c2b40e0a2e7b4c66db729655074479165147b7
humza-sami <humza.sami@primis.ai>
2024-11-19 16:53:47 +0500

    feat: added backbone architecture

---
+++ example.py
@@ -0,0 +1,17 @@
+from ai import Agent, Orchestrator
+
+llm_config = {'model': 'gpt-4o', 'api_key': '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'}
+
+planner = Agent(name="planner",
+                system_message="You are professional verilog testplanner who write testplan when user ask for code",
+                llm_config=llm_config)
+coder = Agent(name="Coder", system_message="You are professional verilog programmer", llm_config=llm_config)
+debugger = Agent(name="Debugger", system_message="You are professional debugger programmer", llm_config=llm_config)
+orchestrator = Orchestrator(name="Ochestrator", llm_config=llm_config)
+
+orchestrator.system_message = "Think you are a hardware design center manager who controls other agents. " + orchestrator.system_message
+
+orchestrator.register_agent(planner)
+orchestrator.register_agent(coder)
+orchestrator.register_agent(debugger)
+orchestrator.start_interactive_session()
