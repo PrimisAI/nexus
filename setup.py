c67de664fcb68fd31252cde94ab5c97c84bedac1
Valerio Tenace <valerio.tenace@gmail.com>
2024-11-24 15:55:06 +0100

    feat: enabled continuous delivery (#6)
    * moved config to `pyproject.toml` and simplified `setup.py` to handle requirements only
    
    * new package `__init__`
    
    * action for continuous delivery

--- setup.py
+++ setup.py
@@ -1,24 +1,10 @@
-from setuptools import setup, find_packages
+import os
+from setuptools import setup
 
-# Read the contents of your requirements.txt file
-with open('requirements.txt') as f:
-    requirements = f.read().splitlines()
+if os.path.exists("requirements.txt"):
+    with open("requirements.txt") as f:
+        requirements = f.read().splitlines()
+else:
+    requirements = []
 
-# Read the contents of your README file
-with open('README.md', 'r', encoding='utf-8') as f:
-    long_description = f.read()
-
-setup(
-    name='primisai',
-    version='0.1.0',
-    author='PrimisAI',
-    author_email='info@primis.ai',
-    description='Nexus is a powerful and flexible Python package for managing AI agents and coordinating complex tasks using LLMs. It provides a robust framework for creating, managing, and interacting with multiple specialized AI agents under the supervision of a central coordinator.',
-    long_description=long_description,
-    long_description_content_type='text/markdown',
-    url='https://github.com/PrimisAI/nexus',
-    packages=find_packages(exclude=['tests*']),
-    install_requires=requirements,
-    python_requires='>=3.10',
-    include_package_data=True,
-)
+setup(install_requires=requirements,)
