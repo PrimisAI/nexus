7bc872399279d361974fe7bd27c396b91fb50881
Mubashir <mubashir54000@gmail.com>
2024-12-04 18:09:50 +0500

    chore: updated 📝 example with latest changes in Supervisor class 👨

--- examples/eda.py
+++ examples/eda.py
@@ -4,10 +4,13 @@ from dotenv import load_dotenv
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from primisai.nexus.core import Agent, Supervisor
 
-
 load_dotenv()
 
-llm_config = {'model': os.getenv('LLM_MODEL'), 'api_key': os.getenv('LLM_API_KEY'), 'base_url': os.getenv('LLM_BASE_URL')}
+llm_config = {
+    'model': os.getenv('LLM_MODEL'), 
+    'api_key': os.getenv('LLM_API_KEY'), 
+    'base_url': os.getenv('LLM_BASE_URL')
+    }
 
 def execute_command(argument: str):
     try:
@@ -62,26 +65,15 @@ debugger = Agent(
     tools=tools,
     use_tools=True)
 
-writer = Agent(
-    name="Writer",
-    system_message="You are a professional Writer.",
+# Initialize supervisor
+supervisor = Supervisor(
+    name="Supervisor", 
+    system_message="Think you are a hardware design center manager who controls other agents.",
     llm_config=llm_config)
 
-
-# Initialize sub-supervisor
-sub_supervisor = Supervisor(name="SubSupervisor", llm_config=llm_config)
-sub_supervisor.system_message = ("You are a sub-supervisor, and you assist the supervisor in writing appropriate emails.")
-sub_supervisor.register_agent(writer)
-
-
-# Initialize supervisor
-supervisor = Supervisor(name="Supervisor", llm_config=llm_config)
-supervisor.system_message = ("Think you are a hardware design center manager who controls other agents. " +
-                               supervisor.system_message)
 supervisor.register_agent(planner)
 supervisor.register_agent(coder)
 supervisor.register_agent(debugger)
-supervisor.register_agent(sub_supervisor)
 
-# supervisor.display_agent_graph()
+supervisor.display_agent_graph()
 supervisor.start_interactive_session()
